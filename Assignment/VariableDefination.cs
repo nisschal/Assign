a3e808edb0ab6a11ad6d4c41fa7a99c44442a091
Nischal Gyawali <nischal.gyawali14@gmail.com>
2019-12-02 22:09:41 +0545

    Variable Class Added

---
+++ Assignment/VariableDefination.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Assignment
+{
+    public class VariableDefination
+    {
+        /// <summary>
+        /// method to check if variable is alphabetical or not
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public bool isAlphabeticalVariable(String word)
+        {
+            if (Regex.IsMatch(word, @"^[a-zA-Z]+$"))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Assignment/Variables.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class Variables
+    {
+        /// <summary>
+        /// settter and getter
+        /// </summary>
+        public string variable { get; set; }
+        public float value { get; set; }
+
+
+        /// <summary>
+        /// variable setter
+        /// </summary>
+        /// <param name="variable"></param>
+        public void setVariable(string variable)
+        {
+            this.variable = variable;
+        }
+
+
+        /// <summary>
+        /// variable getter
+        /// </summary>
+        /// <returns></returns>
+        public string getVariable()
+        {
+            return this.variable;
+        }
+
+
+        /// <summary>
+        /// value setter
+        /// </summary>
+        /// <param name="value"></param>
+        public void setValue(float value)
+        {
+            this.value = value;
+        }
+
+
+        /// <summary>
+        /// value getter
+        /// </summary>
+        /// <returns></returns>
+        public float getValue()
+        {
+            return this.value;
+        }
+    }
+}
