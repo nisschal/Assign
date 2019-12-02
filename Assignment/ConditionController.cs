9b95a135296c07e60e874a37a28fdf68acce52c2
Nischal Gyawali <nischal.gyawali14@gmail.com>
2019-12-02 22:24:44 +0545

    Condition Controller Added

---
+++ Assignment/ConditionController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class ConditionController
+    {
+        //declaration
+        string variable;
+        float value;
+
+        /// <summary>
+        /// variable setter
+        /// </summary>
+        /// <param name="variable"></param>
+        public void setVariable(string variable)
+        {
+            this.variable = variable;
+        }
+
+        /// <summary>
+        /// variable getter
+        /// </summary>
+        /// <returns></returns>
+        public string getVariable()
+        {
+            return this.variable;
+        }
+
+        /// <summary>
+        /// value setter
+        /// </summary>
+        /// <param name="value"></param>
+        public void setValue(float value)
+        {
+            this.value = value;
+        }
+
+        /// <summary>
+        /// value getter
+        /// </summary>
+        /// <returns></returns>
+        public float getValue()
+        {
+            return this.value;
+        }
+    }
+}
