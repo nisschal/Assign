3ce7d9457a9c70234a6475a2e221aeb4b50944f0
Nischal Gyawali <nischal.gyawali14@gmail.com>
2019-12-02 22:21:53 +0545

    Factory Class Added

---
+++ Assignment/FactoryProducer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class FactoryProducer
+    {
+        /// <summary>
+        /// method to create factory of different type like shape, color
+        /// </summary>
+        /// <param name="choice"></param>
+        /// <returns></returns>
+        public static AbstractFactory getFactory(String choice)
+        {
+            //if condition to check if choice is shape or color
+            if (choice.Equals("Shape"))
+            {
+                return new ShapeFactory();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Assignment/FactoryProducerDefination.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class FactoryProducerDefination
+    {
+        /// <summary>
+        /// checks whether the type is shape or color
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public bool isShape(string type)
+        {
+            if (type == "shape")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
