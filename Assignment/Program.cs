4fe1a2aa59041fca8ee958d913a7336cbfa2f00b
Nischal Gyawali <nischal.gyawali14@gmail.com>
2020-01-15 04:13:35 +0545

    Methods Added

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -9,16 +9,35 @@ namespace Assignment
 /// Entry Point of Application
 /// </summary>
     static class Program
-    {
+    {   
+        /// <summary>
+        /// Method without Parameters
+        /// </summary>
+        static void MyMethod()
+        {
+            Console.WriteLine("Program has been Executed.");
+            MessageBox.Show("Press Ok to Continue...");
+        }
+        /// <summary>
+        /// Method with Parameters
+        /// </summary>
+        /// <param name="fname"></param>
+        static void Method1(string fname)
+        {
+            Console.WriteLine("This Application is Created By " + fname + ".");
+        }
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            MyMethod();
+            Method1("Nischal Gyawali");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
         }
     }
 }
